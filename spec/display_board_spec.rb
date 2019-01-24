d3490f85d460b63913660cf93f5b5a6f12d59ec4
jamieberrier <berrier.jamie@gmail.com>
2019-01-24 19:50:18 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -183,6 +183,10 @@ describe "#display_board in 'lib/display_board.rb" do
       # and make a few simple edits to convert the previous example to this
       # example's situation.
       board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"] 
+      
+      # Don't touch the following lines.
+      output = capture_puts{ display_board(board) } if defined?(display_board)
+      rows = output.split("\n")
 
       # *** Comment the line below by adding a # at the line start ***
       #expect(true).to be(true)
