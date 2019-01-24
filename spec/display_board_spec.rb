8bc03676a5d4b24f7d7a95dcce0ba16378e61792
jamieberrier <berrier.jamie@gmail.com>
2019-01-24 19:50:21 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -182,8 +182,8 @@ describe "#display_board in 'lib/display_board.rb" do
       # Hint: You should be able to copy the code in the previous it example
       # and make a few simple edits to convert the previous example to this
       # example's situation.
-      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"] 
-      
+      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+
       # Don't touch the following lines.
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
